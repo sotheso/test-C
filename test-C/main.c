b6ecb91ed2f5be5fd41229b64bb9bfa63d741b75
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-30 17:02:12 +0330

    Initial Commit

---
+++ test-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test-C
+//
+//  Created by Sothesom on 10/10/1403.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
